7637df5b469463e0913096b634f01d34e1e971f4
Vidhya Devkar <vidyadeokar24@gmail.com>
2024-10-18 15:18:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+
+
+# 1. Library imports
+import uvicorn
+from fastapi import FastAPI
+from BankNotes import BankNote
+import numpy as np
+import pickle
+import pandas as pd
+
+# 2. Create the app object
+app = FastAPI()
+
+pickle_in = open("classifier.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+# 3. Index route, opens automatically on http://127.0.0.1:8000  OR http://127.0.0.1:8000/docs
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
+
+# 4. Route with a single parameter, returns the parameter within a message
+#    Located at: http://127.0.0.1:8000/rohit
+@app.get('/{name}')
+def get_name(name: str):
+    return {'Welcome To Fake Currency Model , hi': f'{name}'}
+
+# 3. Expose the prediction functionality, make a prediction from the passed
+#    JSON data and return the predicted Bank Note with the confidence
+@app.post('/predict')
+def predict_banknote(data:BankNote):
+    print(type(data))
+    print(data)
+    data = data.model_dump()
+    variance=data['variance']
+    skewness=data['skewness']
+    curtosis=data['curtosis']
+    entropy=data['entropy']
+    print(classifier.predict([[variance,skewness,curtosis,entropy]]))
+    prediction = classifier.predict([[variance,skewness,curtosis,entropy]])
+    if(prediction[0]==1):
+        prediction="Fake note"
+    else:
+        prediction="Its a Bank note"
+    return {
+        'prediction': prediction
+    }
+    
+# 5. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+    
+#uvicorn app:app --reload
